ad1353d24ca574fa8eb71f91605a77500c013a19
fujikei <aloguefujita@gmail.com>
2014-01-03 14:32:40 +0900

    init commit

---
+++ demp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demp::Application.config.secret_key_base = '50ed1b7177e50ec6a36c13de8f4a5aa840182447edc2770f3a270237641afbf70c972e16cc0190277c7a17e6445ff0c14dd5081a42f2020e0e62f5ecb48b7792'
---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '28b6a360ee6cb97543c355f3479d807b69c1998d246815a1d0ce4898d5c69f056cce8914e99021fd82fba5855a33cd487dda6f65a66b327b60ade604942877eb'
